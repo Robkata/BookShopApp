310912562e0f34f1e63133714355277644ae22f1
Robkata <r.georgiev2017@pmg-pernik.com>
2022-09-20 14:26:52 +0300

    Added final changes

---
+++ BookShopSchool/BookShopSchool/Data/Models/Author.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSchool.Data.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            this.AuthorBooks = new HashSet<AuthorBook>();
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [MinLength(3)]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public ICollection<AuthorBook> AuthorBooks { get; set; }
+    }
+}
